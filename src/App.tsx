87641f59cb8602ee5f7f5438d0fb21429595a4b3
PonVignesh <ponvignesh6@gmail.com>
2026-03-08 02:09:01 +0530

    Added Nav bar and Footer successfully

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AboutMain from "./pages/about/AboutMain";
 import ContactMain from "./pages/contact/ContactMain";
 import HomeMain from "./pages/home/HomeMain";
+import NavBar from "./components/shared/NavBar";
+import Footer from "./components/shared/Footer";
 
 const App = () => {
   return (
-    <div className="">
-      <HomeMain />
-      <AboutMain />
-      <ContactMain />
-    </div>
-  )
-}
-
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomeMain />} />
+        <Route path="/about" element={<AboutMain />} />
+        <Route path="/contact" element={<ContactMain />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+};
 
 export default App;
--- src/pages/home/HomeMain.tsx
+++ src/pages/home/HomeMain.tsx
@@ -1,7 +1,28 @@
+import NavBar from "../../components/shared/NavBar";
+import HeroImage from "../../assets/heroImage.jpg";
 const HomeMain = () => {
     return (
-        <div className="home-main">
-            <h1>Home Main</h1>
+        <div className="home-main w-full">
+            {/* <NavBar /> */}
+            <section className="w-full mb-50 bg-[#1f6f5e] flex flex-col md:flex-row items-center gap-10 md:gap-[10%] px-[5%] md:px-[10%] py-10">
+                <div className="flex flex-col gap-8 flex-1 min-w-[250px] max-w-[600px]">
+                    <p style={{ fontFamily: "Playfair Display, serif" }} className="text-white text-[40px] md:text-[60px] font-bold leading-10 md:leading-20">
+                        Empowering
+                        Education,
+                        Enriching Lives
+                    </p>
+                    <p className="text-[18px] md:text-[20px] text-white">
+                        Supporting underprivileged and parent-less students with educational assistance, school uniforms, and essential learning materials.
+                    </p>
+                    <nav className="z-10 flex gap-5">
+                        <button className="bg-[#284e47] text-white px-6 py-3 rounded-[8px] hover:cursor-pointer hover:bg-[#0f3d35] hover:-translate-y-1 mt-4 transition-transform duration-400">Learn more</button>
+                        <button className="bg-white text-[#104037] px-6 py-3 rounded-[8px] hover:cursor-pointer mt-4 hover:-translate-y-1 transition-transform duration-400">Get involved</button>
+                    </nav>
+                </div>
+                <div className="flex-1 flex justify-center items-center min-w-[220px] max-w-[450px] w-full md:w-[95%] h-auto aspect-[4/3] md:aspect-auto rounded-2xl overflow-hidden hover:scale-108 transition-transform duration-400">
+                    <img src={HeroImage} alt="Hero" className="w-full h-full object-cover max-h-[320px] md:max-h-[400px]" />
+                </div>
+            </section>
         </div>
     )
 }
