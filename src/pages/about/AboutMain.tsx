2bf10648880fdc6a9b8bf1aad360503a5d65547b
PonVignesh <ponvignesh6@gmail.com>
2026-03-07 17:14:18 +0530

    Initialized all folders

---
+++ src/pages/about/AboutMain.tsx
@@ -0,0 +1,9 @@
+const AboutMain = () => {
+    return (
+        <div className="about-main">
+            <h1>About Main</h1>
+        </div>
+    )
+}
+
+export default AboutMain;
---
+++ src/pages/home/HomeMain.tsx
@@ -0,0 +1,9 @@
+const HomeMain = () => {
+    return (
+        <div className="home-main">
+            <h1>Home Main</h1>
+        </div>
+    )
+}
+
+export default HomeMain;
